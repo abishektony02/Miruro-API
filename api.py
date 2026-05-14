99bba1cad7a0423099af4cf2f822612a7902967a
abishektony02 <abishektony02@gmail.com>
2026-05-14 14:58:39 -0400

    Update api.py

--- api.py
+++ api.py
@@ -7,10 +7,7 @@ app = FastAPI(title="Miruro API", version="2.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://id-preview--8c445a7d-9a9c-4886-875f-3349d286a51b.lovable.app",
-        "http://localhost:3000",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
