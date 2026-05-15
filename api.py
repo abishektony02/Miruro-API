23362f9b482d72df6884ce9cafb38d1b9197745a
abishektony02 <abishektony02@gmail.com>
2026-05-15 17:57:18 -0400

    Update api.py

--- api.py
+++ api.py
@@ -10,9 +10,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost",
-        "http://localhost:8080",
+        "http://localhost:3000",
         "http://127.0.0.1",
-        "http://127.0.0.1:8080",
+        "http://127.0.0.1:3000",
         "https://abiwatch.vercel.app"
     ],
     allow_credentials=True,
