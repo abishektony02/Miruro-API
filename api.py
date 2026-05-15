b70e714094f669d99fd7ed865211745f5bc75619
abishektony02 <abishektony02@gmail.com>
2026-05-15 17:46:22 -0400

    Update api.py

--- api.py
+++ api.py
@@ -8,7 +8,13 @@ app = FastAPI(title="Miruro API", version="2.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost",
+        "http://localhost:8080",
+        "http://127.0.0.1",
+        "http://127.0.0.1:8080",
+        "https://abiwatch.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
